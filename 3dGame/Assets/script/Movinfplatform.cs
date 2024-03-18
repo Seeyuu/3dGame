868e16749ae52e9794fc777c59a4013c6faab514
Ployphuk <ployphuk.jafarx@mail.kmutt.ac.th>
2024-03-18 11:00:37 +0700

    Fix : Moving platform

--- 3dGame/Assets/script/Movinfplatform.cs
+++ 3dGame/Assets/script/Movinfplatform.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59d22a3b25b1d740abfbb52049b3e19cb3f40c40266dafeeff71451037d2c682
-size 981
+oid sha256:c70da4f604fa9fc8609cac28e979dfda308c86cc34b4797d00aaf4bf55fc3d4e
+size 1279
